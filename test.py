793b3476ebbcf7a19c3baf8963229e5a27e74caf
Tim Empringham <tim.empringham@live.ca>
2021-02-24 08:52:06 -0500

    First commit.

---
+++ test.py
@@ -0,0 +1,30 @@
+import asyncio
+from tastyigniter import TastyIgniter
+
+async def main():
+    """Testing function."""
+    
+    username = input("Username:")
+    password = input("Password:")
+    domain = input("Domain:")
+
+    api = TastyIgniter(username, password, domain)
+
+    response = await api.get_locations()
+    save_file("locations",str(response))
+
+    response = await api.get_orders()
+    save_file("orders",str(response))
+
+    response = await api.get_received_orders()
+    save_file("received_orders",str(response))
+
+    await api.session.close()
+
+def save_file(fileroot:str,data:str):
+    """Save test output to file."""
+    f = open(f"test_files/{fileroot}.json", "w")
+    f.write(data)
+    f.close()
+
+asyncio.run(main())
